a8c4c2df5ead37a9c811a16305babdfb3c54b483
Yohanes <yohanesdahlan@gmail.com>
2026-06-06 13:35:44 +0700

    database connection

---
+++ config/database.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() *gorm.DB {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	db, err := gorm.Open(
+		postgres.Open(dsn),
+		&gorm.Config{},
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to datavase successfully")
+
+	return db
+}
